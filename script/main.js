d30609015ae1b5a04a56a5527e147fee5722bec7
Tanuj Rajput <99263458+anotherproton@users.noreply.github.com>
2025-01-14 19:36:28 +0530

    Update main.js

--- script/main.js
+++ script/main.js
@@ -7,7 +7,7 @@ window.addEventListener('load', () => {
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
         confirmButtonText: 'Ofcourse',
-        cancelButtonText: 'Yessssss',
+        cancelButtonText: 'Dont Say No',
     }).then((result) => {
         if (result.isConfirmed) {
             document.querySelector('.song').play();
